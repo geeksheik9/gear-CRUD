f9e9c7385f35f31298b85d76ccab643b5fa027e6
Ben Abrams <Benjamin.Abrams@Centene.com>
2020-11-20 10:58:42 -0500

    updated gear CRUD functions

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -2,10 +2,19 @@ package db
 
 import (
 	"context"
+	"errors"
+	"net/url"
+	"strconv"
+	"time"
 
 	model "github.com/geeksheik9/gear-CRUD/models"
+	"github.com/geeksheik9/gear-CRUD/pkg/api"
 	"github.com/sirupsen/logrus"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
@@ -33,6 +42,102 @@ func (g *GearDB) InsertArmor(armor *model.Armor) error {
 	collection := g.client.Database(g.databaseName).Collection(g.armorCollection)
 
 	_, err := collection.InsertOne(context.Background(), armor)
+
+	return err
+}
+
+//GetArmor is the database implementation to get all armor objects
+func (g *GearDB) GetArmor(queryParams url.Values) ([]model.Armor, error) {
+	logrus.Debug("BEGIN - GetArmor")
+
+	collection := g.client.Database(g.databaseName).Collection(g.armorCollection)
+
+	pageNumber, pageCount, sort, filter := api.BuildFilter(queryParams)
+	skip := 0
+	if pageNumber > 0 {
+		skip = (pageNumber - 1) * pageCount
+	}
+
+	opts := options.Find().
+		SetMaxTime(30 * time.Second).
+		SetSkip(int64(skip)).
+		SetLimit(int64(pageCount)).
+		SetSort(bson.D{{
+			Key:   sort,
+			Value: 1,
+		}})
+
+	cur, err := collection.Find(context.Background(), filter, opts)
+	if err != nil {
+		return nil, err
+	}
+
+	matches := []model.Armor{}
+
+	for cur.Next(context.Background()) {
+		elem := model.Armor{}
+		err := cur.Decode(&elem)
+		if err != nil {
+			return nil, err
+		}
+
+		matches = append(matches, elem)
+	}
+
+	return matches, nil
+}
+
+//GetArmorByID is the database implementation to get a pspecific armor back from the database
+func (g *GearDB) GetArmorByID(mongoID primitive.ObjectID) (*model.Armor, error) {
+	logrus.Debugf("BEGIN - GetArmorByID: %v", mongoID)
+
+	collection := g.client.Database(g.databaseName).Collection(g.armorCollection)
+	query := api.BuildQuery(&mongoID, nil)
+
+	sheet := model.Armor{}
+
+	err := collection.FindOne(context.Background(), query).Decode(&sheet)
+	if err != nil {
+		return nil, err
+	}
+
+	return &sheet, err
+}
+
+//UpdateArmorByID updates a specific armor in the armor database
+func (g *GearDB) UpdateArmorByID(armor model.Armor, mongoID primitive.ObjectID) error {
+
+	collection := g.client.Database(g.databaseName).Collection(g.armorCollection)
+
+	result, err := collection.UpdateOne(context.Background(), bson.M{"_id": mongoID}, bson.D{{
+		Key:   "$set",
+		Value: armor,
+	}})
+	if err != nil {
+		return err
+	}
+
+	matched := strconv.FormatInt(result.MatchedCount, 10)
+	modified := strconv.FormatInt(result.ModifiedCount, 10)
+
+	if result.MatchedCount != 1 {
+		return errors.New("Could not update sheet. Tried to update " + mongoID.Hex() + " got " + matched + " matches instead of 1")
+	}
+
+	if result.ModifiedCount != 1 {
+		return errors.New("Could not update sheet. Tried to updated " + mongoID.Hex() + " tried to update " + modified + " number of results instead of 1")
+	}
+
+	return nil
+}
+
+//DeleteArmorByID deletes a specific armor from the database
+func (g *GearDB) DeleteArmorByID(mongoID primitive.ObjectID) error {
+	logrus.Debugf("BEGIN - DeleteForceCharacterSheetByID: %v", mongoID)
+
+	collection := g.client.Database(g.databaseName).Collection(g.armorCollection)
+
+	_, err := collection.DeleteOne(context.Background(), bson.M{"_id": mongoID})
 
 	return err
 }
--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"encoding/json"
 	"net/http"
+	"net/url"
 
 	model "github.com/geeksheik9/gear-CRUD/models"
 	"github.com/geeksheik9/gear-CRUD/pkg/api"
@@ -13,8 +14,15 @@ import (
 
 // GearDatabase is the interface for the actual database object
 type GearDatabase interface {
+	//Armor methods
 	InsertArmor(armor *model.Armor) error
+	GetArmor(query url.Values) ([]model.Armor, error)
+	GetArmorByID(mongoID primitive.ObjectID) (*model.Armor, error)
+	UpdateArmorByID(armor model.Armor, mongoID primitive.ObjectID) error
+	DeleteArmorByID(mongoID primitive.ObjectID) error
+	//Weapon methods
 	InsertWeapon(weapon *model.Weapon) error
+	//Helper methods
 	Ping() error
 }
 
@@ -34,12 +42,17 @@ func (s *GearService) Routes(r *mux.Router) *mux.Router {
 	r.HandleFunc("/weapon", s.InsertWeapon).Methods(http.MethodPost)
 
 	//TODO: GETALL
+	r.HandleFunc("/armor", s.GetArmor).Methods(http.MethodGet)
 
 	//TODO: GETBYID
+	r.HandleFunc("/armor/{ID}", s.GetArmorByID).Methods(http.MethodGet)
 
 	//TODO: UPDATEBYID
+	r.HandleFunc("/armor/{ID}", s.UpdateArmorByID).Methods(http.MethodPut)
 
 	//TODO: DELETEBYID
+	r.HandleFunc("/armor/{ID}", s.DeleteArmorByID).Methods(http.MethodDelete)
+
 	return r
 }
 
@@ -115,4 +128,90 @@ func (s *GearService) InsertWeapon(w http.ResponseWriter, r *http.Request) {
 	}
 
 	api.RespondWithJSON(w, http.StatusOK, "Weapon Object Created")
+}
+
+//GetArmor is the handler function to return all armor in the database
+func (s *GearService) GetArmor(w http.ResponseWriter, r *http.Request) {
+	logrus.Infof("GetArmor invoked with url: %v", r.URL)
+
+	armor, err := s.Database.GetArmor(r.URL.Query())
+	if err != nil {
+		api.RespondWithError(w, api.CheckError(err), err.Error())
+		return
+	}
+
+	api.RespondWithJSON(w, http.StatusOK, armor)
+}
+
+//GetArmorByID is the handler function to return a specific armor in the database
+func (s *GearService) GetArmorByID(w http.ResponseWriter, r *http.Request) {
+	logrus.Infof("GetArmorByID invoked with url: %v", r.URL)
+
+	vars := mux.Vars(r)
+	ID := vars["ID"]
+
+	objectID, err := api.StringToObjectID(ID)
+	if err != nil {
+		api.RespondWithError(w, api.CheckError(err), err.Error())
+		return
+	}
+
+	sheet, err := s.Database.GetArmorByID(objectID)
+	if err != nil {
+		api.RespondWithError(w, api.CheckError(err), err.Error())
+		return
+	}
+
+	api.RespondWithJSON(w, http.StatusOK, sheet)
+}
+
+//UpdateArmorByID is the handler function to update a specific armor in the database
+func (s *GearService) UpdateArmorByID(w http.ResponseWriter, r *http.Request) {
+	logrus.Infof("UpdateArmorByID invoked with url: %v", r.URL)
+	defer r.Body.Close()
+
+	vars := mux.Vars(r)
+	ID := vars["ID"]
+
+	objectID, err := primitive.ObjectIDFromHex(ID)
+	if err != nil {
+		api.RespondWithError(w, api.CheckError(err), err.Error())
+		return
+	}
+
+	armor := model.Armor{}
+	err = json.NewDecoder(r.Body).Decode(&armor)
+	if err != nil {
+		api.RespondWithError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	err = s.Database.UpdateArmorByID(armor, objectID)
+	if err != nil {
+		api.RespondWithError(w, api.CheckError(err), err.Error())
+		return
+	}
+
+	api.RespondWithJSON(w, http.StatusOK, objectID)
+}
+
+//DeleteArmorByID is the handler function to remove a specific armor in the database
+func (s *GearService) DeleteArmorByID(w http.ResponseWriter, r *http.Request) {
+	logrus.Infof("DeleterArmorByID invoked with url: %v", r.URL)
+
+	vars := mux.Vars(r)
+	ID := vars["ID"]
+
+	objectID, err := primitive.ObjectIDFromHex(ID)
+	if err != nil {
+		api.RespondWithError(w, api.CheckError(err), err.Error())
+		return
+	}
+
+	err = s.Database.DeleteArmorByID(objectID)
+	if err != nil {
+		api.RespondWithError(w, api.CheckError(err), err.Error())
+	}
+
+	api.RespondNoContent(w, http.StatusNoContent)
 }
