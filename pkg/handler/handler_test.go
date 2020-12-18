10ea25716795829af263e9806eadf3c031490061
Ben Abrams <Benjamin.Abrams@Centene.com>
2020-12-18 12:58:39 -0500

    updated tests for insert armor

--- pkg/handler/handler_test.go
+++ pkg/handler/handler_test.go
@@ -1,6 +1,8 @@
 package handler
 
 import (
+	"bytes"
+	"encoding/json"
 	"errors"
 	"net/http"
 	"net/http/httptest"
@@ -9,6 +11,7 @@ import (
 	model "github.com/geeksheik9/gear-CRUD/models"
 	"github.com/geeksheik9/gear-CRUD/pkg/db/mocks"
 	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func InitMockGearService(armor *model.Armor, armors []model.Armor, weapon *model.Weapon, weapons []model.Weapon, errors error) (s GearService) {
@@ -26,15 +29,39 @@ func InitMockGearService(armor *model.Armor, armors []model.Armor, weapon *model
 	}
 }
 
-func mockWeapon() {
-
+func mockWeapon(id primitive.ObjectID, name string, price int64) model.Weapon {
+	weapon := model.Weapon{
+		ID:    id,
+		Name:  name,
+		Price: price,
+	}
+	return weapon
 }
 
-func mockArmor() {
-
+func mockWeapons(weapon model.Weapon) []model.Weapon {
+	weapons := []model.Weapon{
+		weapon,
+	}
+	return weapons
 }
 
-func TestCharacterService_PingCheck(t *testing.T) {
+func mockSingleArmor(id primitive.ObjectID, armorType string, price int64) model.Armor {
+	armor := model.Armor{
+		ID:        id,
+		ArmorType: armorType,
+		Price:     price,
+	}
+	return armor
+}
+
+func mockArmor(armor model.Armor) []model.Armor {
+	armors := []model.Armor{
+		armor,
+	}
+	return armors
+}
+
+func TestGearService_PingCheck(t *testing.T) {
 	service := InitMockGearService(nil, nil, nil, nil, nil)
 
 	r, err := http.NewRequest("GET", "/ping", nil)
@@ -50,7 +77,7 @@ func TestCharacterService_PingCheck(t *testing.T) {
 	}
 }
 
-func TestCharacterService_HealthCheck(t *testing.T) {
+func TestGearService_HealthCheck(t *testing.T) {
 	service := InitMockGearService(nil, nil, nil, nil, nil)
 	r, err := http.NewRequest("GET", "/health", nil)
 	if err != nil {
@@ -64,7 +91,7 @@ func TestCharacterService_HealthCheck(t *testing.T) {
 	}
 }
 
-func TestCharacterService_HealthCheck_error(t *testing.T) {
+func TestGearService_HealthCheck_error(t *testing.T) {
 	service := InitMockGearService(nil, nil, nil, nil, errors.New("test error"))
 	r, err := http.NewRequest("GET", "/health", nil)
 	if err != nil {
@@ -75,5 +102,65 @@ func TestCharacterService_HealthCheck_error(t *testing.T) {
 	service.Routes(router).ServeHTTP(w, r)
 	if w.Code != http.StatusFailedDependency {
 		t.Errorf("Ping() error:\ngot: %v\n expected: %v", w.Code, http.StatusFailedDependency)
+	}
+}
+
+func TestGearService_InsertArmor_Success(t *testing.T) {
+	id := primitive.NewObjectID()
+	armor := mockSingleArmor(id, "test", 5)
+	service := InitMockGearService(&armor, nil, nil, nil, nil)
+
+	request, _ := json.Marshal(armor)
+
+	r, err := http.NewRequest("POST", "/armor", bytes.NewBuffer(request))
+	if err != nil {
+		t.Errorf("InsertArmor() error creating request:\ngot: %v\nexpected: <no error>", err)
+	}
+
+	w := httptest.NewRecorder()
+	router := mux.NewRouter().StrictSlash(true)
+	service.Routes(router).ServeHTTP(w, r)
+	if w.Code != http.StatusOK {
+		t.Errorf("InsertArmor() error:\ngot: %v\nexpected: %v", w.Code, http.StatusOK)
+	}
+}
+
+func TestGearService_InsertArmor_DBError(t *testing.T) {
+	id := primitive.NewObjectID()
+	armor := mockSingleArmor(id, "test", 5)
+	service := InitMockGearService(nil, nil, nil, nil, errors.New("test error"))
+
+	request, _ := json.Marshal(armor)
+
+	r, err := http.NewRequest("POST", "/armor", bytes.NewBuffer(request))
+	if err != nil {
+		t.Errorf("InsertArmor() error creating request:\ngot: %v\nexpected: <no error>", err)
+	}
+
+	w := httptest.NewRecorder()
+	router := mux.NewRouter().StrictSlash(true)
+	service.Routes(router).ServeHTTP(w, r)
+
+	if w.Code != http.StatusInternalServerError {
+		t.Errorf("InsertArmor() error:\ngot: %v\nexpected:%v", w.Code, http.StatusInternalServerError)
+	}
+}
+
+func TestGearService_InsertArmor_BadJSON(t *testing.T) {
+	service := InitMockGearService(nil, nil, nil, nil, nil)
+
+	request, _ := json.Marshal(`{bad json`)
+
+	r, err := http.NewRequest("POST", "/armor", bytes.NewBuffer(request))
+	if err != nil {
+		t.Errorf("InsertArmor() error creating request:\ngot: %v\nexpected: <no error>", err)
+	}
+
+	w := httptest.NewRecorder()
+	router := mux.NewRouter().StrictSlash(true)
+	service.Routes(router).ServeHTTP(w, r)
+
+	if w.Code != http.StatusBadRequest {
+		t.Errorf("InsertArmor() error:\ngot: %v\nexpected: %v", w.Code, http.StatusBadRequest)
 	}
 }
